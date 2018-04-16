6ed8b68f695ff82298b214ccda3eccaec7a1032f
Crystal To <crystal.kyto@gmail.com>
2018-04-15 23:03:17 -0700

    Add tooltip with more info

--- src/Bus.js
+++ src/Bus.js
@@ -4,8 +4,8 @@ import {Marker} from 'react-map-gl';
 
 const Bus = (props) => {
   return (
-    <Marker latitude={props.Latitude} longitude={props.Longitude}>
-      <div className="bus" />
+    <Marker latitude={props.Latitude} longitude={props.Longitude} offsetLeft={-5} offsetTop={-5}>
+      <div className="bus" onClick={props.onClick}/>
     </Marker>
   );
 }
--- src/Map.js
+++ src/Map.js
@@ -3,7 +3,7 @@ import {bindActionCreators} from 'redux';
 import * as busActions from './actions/busActions';
 import Bus from './Bus.js';
 import React, {Component} from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Popup} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 
@@ -14,16 +14,35 @@ class Map extends Component {
       viewport: {
         width: 1000,
         height: 600,
-        latitude: 49.2827,
-        longitude: -123.1207,
+        latitude: 49.2420,
+        longitude: -122.9441,
         zoom: 10
-      }
+      },
+      popup: null
     };
+    this._renderPopup = this._renderPopup.bind(this);
   }
 
   componentWillMount() {
     this.props.busActions.fetchBuses();
     setInterval(this.props.busActions.fetchBuses, 1000);
+  }
+
+  _renderPopup() {
+    const {popup} = this.state;
+
+    return popup && (
+      <Popup tipSize={5}
+        anchor="bottom"
+        longitude={popup.Longitude}
+        latitude={popup.Latitude}
+        onClose={() => this.setState({popup: null})} >
+        <p>Route No: {popup.RouteNo}</p>
+        <p>Destination: {popup.Destination}</p>
+        <p>Direction: {popup.Direction}</p>
+        <p>Last updated: {popup.RecordedTime}</p>
+      </Popup>
+    );
   }
 
   render() {
@@ -33,7 +52,10 @@ class Map extends Component {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         onViewportChange={(viewport) => this.setState({viewport})}
       >
-        {this.props.buses.map((bus) => <Bus {...bus} key={bus.VehicleNo} />)}
+        {this.props.buses.map((bus) =>
+          <Bus {...bus} key={bus.VehicleNo} onClick={() => this.setState({popup: bus})} />
+        )}
+        {this._renderPopup()}
       </ReactMapGL>
     );
   }
